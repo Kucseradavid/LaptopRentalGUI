0ae071caa75728e769203395430cf6b771675bf9
Kucseradavid <kucseradavid02@gmail.com>
2025-05-29 13:09:43 +0200

    proggress

--- LaptopRentalGUI/MainWindow.xaml.cs
+++ LaptopRentalGUI/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +9,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using LaptopRentalGUI.Classes;
 
 namespace LaptopRentalGUI
 {
@@ -19,6 +21,24 @@ namespace LaptopRentalGUI
         public MainWindow()
         {
             InitializeComponent();
+
+            StreamReader olvaso = new StreamReader("../../../laptoprentals2022.csv");
+            olvaso.ReadLine(); //fejléc
+            List<Rent> adatok = new List<Rent>();
+            List<Laptop> laptopok = new List<Laptop>();
+
+            while (!olvaso.EndOfStream)
+            {
+                string sor = olvaso.ReadLine();
+                Rent egyadat = new Rent(sor);
+                adatok.Add(egyadat);
+                Laptop egylaptop = new Laptop(sor);
+                laptopok.Add(egylaptop);
+            }
+
+            olvaso.Close();
+
+            LaptopListView.ItemsSource = adatok;
         }
     }
 }
